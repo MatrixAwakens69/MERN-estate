e32c1b8cd5df2ae8dd1daa25c33383dba68d1733
Ponnanna-AH <siliconguy878@gmail.com>
2023-12-26 20:23:45 +0530

    Removed node_modules directory

--- api/index.js
+++ api/index.js
@@ -3,5 +3,5 @@ import express from "express";
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Listening to port 3000 (server)');
+    console.log('Server listening to port 3000');
 });
