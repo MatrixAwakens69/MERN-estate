cad8de55ca96307e2508b095700ea5716e61012a
Ponnanna-AH <siliconguy878@gmail.com>
2023-12-26 20:42:24 +0530

    Connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log('Connected to db');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 const app = express();
 
