eb91c93436c4802750933ae9917a2a7d47eea811
Ponnanna-AH <siliconguy878@gmail.com>
2023-12-28 15:32:34 +0530

    Completed profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
 
 export default function Profile() {
+  const [formData, setFormData] = useState({});
+  const { loading, error, currentUser } = useSelector(state => state.user);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
+  }
+
   return (
-    <div>Profile</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Profile</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <img className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' src={ currentUser.avatar } alt="profile" />
+        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange} />
+        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
+        <input type="text" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          { loading ? 'Loading...' : 'Update' }
+        </button>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-700 cursor-pointer'>Delete account</span>
+        <span className='text-red-700 cursor-pointer'>Sign out</span>
+      </div>
+      { error && <p className='text-red-500 mt-5'>{error}</p>}
+    </div>
   )
 }
