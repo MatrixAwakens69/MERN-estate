3a9569e715a14bbf6026ea01e4ac3d2de5c1d2c9
Ponnanna-AH <siliconguy878@gmail.com>
2023-12-28 19:36:18 +0530

    Complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,63 +1,68 @@
-import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux';
-import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
-import OAuth from '../components/OAuth';
+import { useState, useRef, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase';
 
 export default function Profile() {
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
-  const { loading, error, currentUser } = useSelector(state => state.user);
 
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const { currentUser } = useSelector(state => state.user);
 
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    try {
-      e.preventDefault();
-      dispatch(signInStart());
-      const res = await fetch('/api/auth/signin', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-      });
-      const data = await res.json();
-      if (data.success === false) {
-        dispatch(signInFailure(data.message));
-        return;
-      }
-      dispatch(signInSuccess(data));
-      navigate('/');
-    } catch (error) {
-      dispatch(signInFailure(error.message));
+  useEffect(() => {
+    if(file) {
+      handleFileUpload(file);
     }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => setFormData({...formData, avatar: downloadURL}))
+      }
+    );
   }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Profile</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <img className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' src={ currentUser.avatar } alt="profile" />
-        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange} />
-        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
-        <input type="text" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-          { loading ? 'Loading...' : 'Update' }
+      <form className='flex flex-col gap-4'>
+        <input onChange={(e) => setFile(e.target.files[0])} type="file" name="" id="" ref={fileRef} hidden accept='image/*' />
+        <img onClick={() => fileRef.current.click()} className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' src={ formData.avatar || currentUser.avatar } alt="profile" />
+        <p className='text-sm self-center'>
+          { fileUploadError ?
+            (<span className='text-red-700'>Image upload error! (image must be smaller than 2mb)</span>) :
+              (filePerc > 0 && filePerc < 100 ? (<span className='text-slate-700'>{`Uploading ${filePerc}`}</span>) :
+                (filePerc === 100 ? (<span className='text-green-700'>Image uploaded successfully!</span>) : ''))
+          }
+        </p>
+        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' />
+        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' />
+        <input type="text" placeholder='password' className='border p-3 rounded-lg' id='password' />
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          Update
         </button>
       </form>
       <div className='flex justify-between mt-5'>
         <span className='text-red-700 cursor-pointer'>Delete account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
-      { error && <p className='text-red-500 mt-5'>{error}</p>}
+      {/* { error && <p className='text-red-500 mt-5'>{error}</p>} */}
     </div>
   )
 }
